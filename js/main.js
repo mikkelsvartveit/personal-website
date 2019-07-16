ecd720c5cb218632bee7160c6a668ce85db7ca6a
Mikkel Svartveit <mikkels@ostfoldfk.no>
2019-07-17 01:56:05 +0200

    you can now click to enlarge photos

--- js/main.js
+++ js/main.js
@@ -9,3 +9,24 @@ window.addEventListener("scroll", function() {
         topButton.classList = "hidden";
     }
 });
+
+var photoElements = document.getElementById("photos-grid").children;
+for(var i = 0; i < photoElements.length; i++) {
+    photoElements[i].addEventListener("click", function(){
+        var photoUrl = this.getAttribute("src").replace("/thumbs/", "/photos/");
+        var fullscreenPhoto = document.getElementById("photo-fullscreen");
+        fullscreenPhoto.children[0].setAttribute("src", photoUrl);
+        document.getElementById("dim").classList = "";
+        fullscreenPhoto.classList = "";
+    });
+}
+
+document.getElementById("closePhotoButton").addEventListener("click", closePhoto);
+
+document.getElementById("dim").addEventListener("click", closePhoto);
+
+function closePhoto() {
+    document.getElementById("dim").classList = "hidden";
+    document.getElementById("photo-fullscreen").classList = "hidden";
+    document.getElementById("photo-fullscreen").children[0].setAttribute("src", "#");
+}
